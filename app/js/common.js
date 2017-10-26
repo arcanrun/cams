00168f43d87a2bd90d79436d6ea4b07c34a7a459
Arkadiy Nazin <arcan.run@gmail.com>
2017-10-27 01:40:34 +0300

    parllax first try

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
-	// Custom JS
+	$(window).scroll(function(){
+
+		var st = $(this).scrollTop();
+
+		$(".header").css({
+			"transform" : "translate(0%, " + st/5 + "%" 
+		});
+
+	});
 
 });
