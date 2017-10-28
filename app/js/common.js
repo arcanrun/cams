76c6e05b70dd6d8f75c67d96ca6391417c89a012
Arkadiy Nazin <arcan.run@gmail.com>
2017-10-28 19:54:23 +0300

    js-block begining

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,29 @@
 $(function() {
+// tabs
+$(".tabs-content > div").not(":first").hide();
+$(".tabs-control a").on("click", function(e){
+	e.preventDefault();
+	$(".tabs-control a").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tabs-content div").hide().eq($(this).index()).fadeIn();
+	console.log($(this).index());
+	console.log($(this).eq());
+});
 
-	// $(window).scroll(function(){
+// mobile
+$("#show-tabs").on("click", function(){
+	$(".tabs-control").fadeToggle();
 
-	// 	var st = $(this).scrollTop();
+});
 
-	// 	$(".header").css({
-	// 		"transform" : "translate(0%, " + st/5 + "%" 
-	// 	});
 
-	// });
+//show blc when widnow's size is wider then 767
+$(window).resize(function() {
+	if($(window).width() > 767)
+    {	
+    	$(".tabs-control").fadeIn();
+      $(".tabs-control").css("display", "block");
+    }
+});
 
+// end tabs
 });
